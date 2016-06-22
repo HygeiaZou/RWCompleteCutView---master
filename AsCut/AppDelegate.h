1639315762a1b702f15fb0fc9c43076665121ea8
RyeWhiskey <zhongyu@zhongyudemacbook-pro.local>
2016-06-22 16:21:47 +0800

    Initial Commit

---
+++ AsCut/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AsCut
+//
+//  Created by zhongyu on 16/6/22.
+//  Copyright © 2016年 RyeWhiskey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
