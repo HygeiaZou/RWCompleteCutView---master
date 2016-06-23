3aea04c89733b23ac1bea64e2062b40b537a7e0c
RyeWhiskey <zhongyu@zhongyudeMacBook-Pro.local>
2016-06-23 18:37:17 +0800

    new
    new

--- AsCut/ViewController.m
+++ AsCut/ViewController.m
@@ -7,8 +7,13 @@
 //
 
 #import "ViewController.h"
+#import "RWCompleteCutView.h"
 
 @interface ViewController ()
+
+<
+    RWCompleteCutViewDelegate
+>
 
 @end
 
@@ -17,6 +22,32 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    NSArray *images = @[[UIImage imageNamed:@"beauty1.jpg"],
+                        [UIImage imageNamed:@"beauty2.jpg"],
+                        [UIImage imageNamed:@"beauty3.jpg"],
+                        [UIImage imageNamed:@"beauty4.jpg"],
+                        [UIImage imageNamed:@"beauty5.jpg"],
+                        [UIImage imageNamed:@"beauty6.jpg"],
+                        [UIImage imageNamed:@"beauty7.jpg"]];
+    
+    RWCompleteCutView *cut = [[RWCompleteCutView alloc] initWithConstraint:^(MASConstraintMaker *make) {
+       
+        make.left.equalTo(self.view.mas_left).offset(0);
+        make.right.equalTo(self.view.mas_right).offset(0);
+        make.top.equalTo(self.view.mas_top).offset(0);
+        make.bottom.equalTo(self.view.mas_bottom).offset(0);
+    }];
+    
+    [self.view addSubview:cut];
+    
+    cut.cardSource = images;
+    cut.eventSource = self;
+}
+
+- (void)buttonDidClickWithIndexPath:(NSIndexPath *)indexPath
+{
+    NSLog(@"%@",indexPath);
 }
 
 - (void)didReceiveMemoryWarning {
